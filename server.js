79a9c65741c8e5d3f838ce58e7abe7b614acb976
JawadMM <almuttawa.j@hotmail.com>
2025-05-09 17:43:12 +0300

    used PM2 to manage clusters

--- server.js
+++ server.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os");
 
 const app = express();
 
@@ -17,19 +15,10 @@ app.get("/", (req, res) => {
 
 app.get("/timer", (req, res) => {
   delay(9000);
-  res.send(`Time is up! ${process.pid}`);
+  res.send(`Delayed... ${process.pid}`);
 });
 
-if (cluster.isMaster) {
-  console.log("Master has been started...");
-  const NUM_WORKERS = os.cpus().length;
-  console.log(NUM_WORKERS);
-  for (let i = 0; i < NUM_WORKERS; i++) {
-    cluster.fork();
-  }
-} else {
-  console.log("Worker process started...");
-  app.listen(3000, () => {
-    console.log("Listening on port 3000...");
-  });
-}
+console.log("Worker process started...");
+app.listen(3000, () => {
+  // console.log("Listening on port 3000...");
+});
